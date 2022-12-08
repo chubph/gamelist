92a6ef9fdc3704c2f930fca64dfaec5dd4408f0c
Filippos Mantzios <mantzios_ph@hotmail.com>
2022-12-08 16:00:56 +0200

    make vue components

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Gamelist from "@/components/GameList.vue";
+</script>
+
+<template>
+ <h2>Hi</h2>
+
+  <gamelist></gamelist>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/GameList.vue
@@ -0,0 +1,15 @@
+<template>
+<h4>This is a game list</h4>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent ({
+  name: "gamelist"
+})
+</script>
+
+<style scoped>
+
+</style>
